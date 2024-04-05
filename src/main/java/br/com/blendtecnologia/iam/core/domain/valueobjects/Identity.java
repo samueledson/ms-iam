a4df7e4d6396c5694705571ba2681c536edf5c16
Samuel Edson <samuelvieira@movida.com.br>
2024-04-05 01:24:37 -0300

    Fixes modifiers in the Identity class.

--- src/main/java/br/com/blendtecnologia/iam/core/domain/valueobjects/Identity.java
+++ src/main/java/br/com/blendtecnologia/iam/core/domain/valueobjects/Identity.java
@@ -5,7 +5,7 @@ import lombok.Value;
 @Value
 public class Identity {
     
-    private final Long number;
+    Long number;
 
     public static Identity nothing() {
         return new Identity(Long.MIN_VALUE);
