008f260f2217f7ffede18c885bc277196ffdc257
Samuel Edson <samuelvieira@movida.com.br>
2024-04-05 01:12:54 -0300

    Add lombok to the UserStatus enum.

--- src/main/java/br/com/blendtecnologia/iam/core/domain/entities/UserStatus.java
+++ src/main/java/br/com/blendtecnologia/iam/core/domain/entities/UserStatus.java
@@ -1,19 +1,17 @@
 package br.com.blendtecnologia.iam.core.domain.entities;
 
+import lombok.Getter;
+
+@Getter
 public enum UserStatus {
     INACTIVE(0L),
     ACTIVE(1L),
     PENDING(2L),
     BLOCKED(3L);
 
-    private Long value;
+    private final Long value;
 
     UserStatus(Long value) {
         this.value = value;
     }
-
-    public Long getValue() {
-        return value;
-    }
-    
 }
